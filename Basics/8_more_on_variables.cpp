2d8df6911b02d648e932eab88c7dc416c9565118
Jocefyneroot <jocefyneroot1582003@gmail.com>
2022-09-24 17:17:13 +0530

    8_more_on_variables

---
+++ Basics/8_more_on_variables.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // more on variables
+
+    // what is variable ?
+    // int a = 12;
+
+    // cout << 15 << endl;
+    // cout << 15 << endl;
+    // cout << 15 << endl;
+    // cout << 15 << endl;
+    // cout << 15 << endl;
+
+    // cout << "With Variable" << endl;
+    // a = 15;
+    // cout << a << endl;
+    // cout << a << endl;
+    // cout << a << endl;
+    // cout << a << endl;
+    // cout << a << endl;
+
+    // int , float, bool, char, double
+
+    int a = 11;
+    float b = 12.43f;
+    char c = 'C';
+    double d = 12.43;
+
+    // 43.43f;
+
+    cout << "Value of a is: " << a + b << endl;
+    cout << "Value of b is: " << b << endl;
+    cout << "Value of c is: " << c << endl;
+    cout << "Value of d is: " << d << endl;
+
+    int e, f, g, h, i;
+
+    e = 1;
+    f = 32;
+    cout << "Value of e is: " << e << endl;
+    cout << "Value of f is: " << f << endl;
+    cout << "Value of g is: " << g << endl;
+
+    return 0;
+}
