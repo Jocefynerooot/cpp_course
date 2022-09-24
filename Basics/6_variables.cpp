a431d14454c77de5603a11ca61cde0c2e681e4a9
Jocefyneroot <jocefyneroot1582003@gmail.com>
2022-09-24 10:02:49 +0530

    6_Intro_to_variables

---
+++ Basics/6_variables.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    cout << "Variables Tutorial" << endl;
+
+    int var = 12;
+
+    cout << 12 << endl;
+    cout << var << endl;
+    
+    var = 14;
+
+    int var2 = var;
+
+    return 0;
+}
