049ae80f089c7fe56a695001703ab483f2c1c431
Jocefyneroot <jocefyneroot1582003@gmail.com>
2022-11-08 12:25:27 +0530

    identifiers

---
+++ Basics/12_identifiers.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // ### The general rules for naming variables are:
+
+    // 1. Names can contain letters, digits and underscores
+    // 2. Names must begin with a letter or an underscore (_)
+    // 3. Names are case sensitive (myVar and myvar are different variables)
+    // 4. Names cannot contain whitespaces or special characters like !, #, %, etc.
+    // 5. Reserved words (like C++ keywords, such as int) cannot be used as names
+
+    // now allowed
+    // int @fruit = 12;
+    // int 2Class = 32;
+    // int house #ew = 43;
+
+    string my_name = "Jocefyneroot";
+    int my_2_class = 43;
+    int _num = 32;
+
+    return 0;
+}
