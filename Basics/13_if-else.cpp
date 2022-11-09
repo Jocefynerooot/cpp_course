f1f2b25931f7183869c37d1af6452e317dc4db61
Jocefyneroot <jocefyneroot1582003@gmail.com>
2022-11-09 12:59:11 +0530

    if else intro

---
+++ Basics/13_if-else.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // sometimes we want to perform some task if some conditions are setisfied
+
+    // template
+    // if (/* condition */)
+    // {
+    //     /* code */
+    // }
+
+    int a = 6;
+    int b = 3;
+
+    if (a < b)
+    {
+        cout << "a is greater than b" << endl;
+    }
+    else
+    {
+        cout << "a is less than b" << endl;
+    }
+
+    return 0;
+}
